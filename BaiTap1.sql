7660689a36f96a861b113979e5ab4d98a261f131
USER <user@USERnoMacBook-Air.local>
2021-09-22 00:46:11 +0900

    Bai tap 1

---
+++ BaiTap1.sql
@@ -0,0 +1,10 @@
+drop database if exists Testing_System_Assignment_1;
+
+create database Testing_System_Assignment_1;
+
+use  Testing_System_Assignment_1;
+
+CREATE TABLE Department (
+    DepartmentID INT PRIMARY KEY AUTO_INCREMENT,
+    DepartmentName VARCHAR(250)
+);
